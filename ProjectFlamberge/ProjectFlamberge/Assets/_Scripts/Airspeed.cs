3fe42aa5a7207458dccb5aa8aac8c082c1427e18
Alexander Hinton <alexandermhinton@gmail.com>
2019-10-12 22:15:25 -0700

    Airspeed
    Added a few lines to FlambergeShip script PlayerController that compare the position of the ship from this frame to the previous and calculate a float called magnitude.
    
    Added a script named Airspeed to canvas GameObject named Speed.
    
    Script updates textmeshproUGUI once a second with the current calculated airspeed

---
+++ ProjectFlamberge/ProjectFlamberge/Assets/_Scripts/Airspeed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class Airspeed : MonoBehaviour
+{
+    //GameObject playerShip;
+    float magnitude;
+    //TextMeshPro text;
+    public TextMeshProUGUI newText;
+    float airspeed;
+    private float updateTimer = 1;
+    public int speedMultiplier;
+
+    public ShipController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerShip = GameObject.FindWithTag("Player");
+        newText = GetComponent<TextMeshProUGUI>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateTimer -= Time.deltaTime;
+
+        if (updateTimer <= 0)
+        {
+            magnitude = playerController.magnitude;
+            airspeed = magnitude * speedMultiplier;
+            newText.text = airspeed.ToString("N0");
+            updateTimer = 1;
+
+        }
+
+    }
+}
